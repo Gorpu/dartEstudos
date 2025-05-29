875257290842e185541ccd72e5936ef78a17a9ab
DEV-LIEDSON <rfliedson007@gmail.com>
2025-05-29 18:03:32 -0300

    Segundo commit

--- au1234567_Resumo.dart
+++ au1234567_Resumo.dart
@@ -1,26 +1,20 @@
-import 'dart:io';
-
-void main() {
-  //Função
-  void Function() nome = () {
-    print("Nome");
-    var n = stdin.readLineSync();
-    print("Idade");
-    var i = stdin.readLineSync();
-    print("Nome:\n-$n\nIdade:\n-$i");
-  };
-
-  //Função Lambda ou anonima
-  Map<String, int> Function(String, int) meuCADASTRO = (p0, p1) => {p0: p1};
-
-  List<dynamic> Function(dynamic) Contador = (p0) {
-    List<dynamic> listNumbers = [];
-    for (int i in listNumbers) {
-      listNumbers.add(i / 2);
-      print(listNumbers);
-      return listNumbers;
-    }
-  };
-  print(Contador.call(10));
-  print(meuCADASTRO("20", 6));
+void main {
+  cadastro("liedson@gmail.com", 63,id: 1, nome: "Liedson", sobrenome: "Rocha");
+}
+void cadastro(
+  String email,
+  num cep,
+  {required id,
+  required String nome,
+  required String sobrenome,
+  dynamic telefone = "Sem Número",
+   }
+){
+  print(
+    "Id: $id"
+    +" Nome: $nome"
+    +" Sobrenome: $sobrenome"
+    +" Telefone: $telefone"
+    +" Email: $email"
+    );
 }
